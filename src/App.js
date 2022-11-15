c830acc3359b39d1a562f21c71059efc3df70d0d
Eric Navar <eric.navar@asurion.com>
2022-11-14 21:02:57 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="augh">
+      Kent Fucks Lovers Blog
+
+      <video width="400" controls>
+        <source src={process.env.REACT_APP_VIDEO} type="video/mp4"/>
+      </video>
     </div>
   );
 }
